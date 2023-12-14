fd2914765e87f48977f49857923ff66efabe41c5
Luis-Felipe-Dev <luis.fhb.2016@gmail.com>
2023-12-14 07:51:26 -0500

    change notification

--- appReservation/views.py
+++ appReservation/views.py
@@ -435,50 +435,50 @@ def pay_with_card(request, id):
     return render(request, 'reservation/pay_with_card.html', context)
 
     
-def send_email_reminder(request):
-    customers = set()
-    reservations = Reservation.objects.filter(date=timezone.now().date(), reminder_sent=False, type_status=2)
+# def send_email_reminder(request):
+#     customers = set()
+#     reservations = Reservation.objects.filter(date=timezone.now().date(), reminder_sent=False, type_status=2)
 
-    for reserv in reservations:
-        start_datetime = datetime.combine(timezone.now().date(), reserv.start_hour)
-        time_difference = start_datetime - timezone.now()
+#     for reserv in reservations:
+#         start_datetime = datetime.combine(timezone.now().date(), reserv.start_hour)
+#         time_difference = start_datetime - timezone.now()
 
-        if timedelta(minutes=10) <= time_difference < timedelta(minutes=11):
-            context = {
-                    'email': User.objects.get(id=reserv.created_user),
-                    'date': reserv.date,
-                    'start_hour': reserv.start_hour,
-                    'end_hour': reserv.end_hour,
-                    'field_soccer_name': reserv.field_soccer.name,
-                    'field_soccer_number_players': reserv.field_soccer.number_players,
-                    'field_soccer_price': reserv.field_soccer.price,
-                    'field_soccer_image_base64': reserv.field_soccer.image_base64.decode("utf-8"),
-                    'establishment_name': reserv.field_soccer.establishment.name,
-                    'establishment_location': reserv.field_soccer.establishment.location,
-                    'establishment_phone': reserv.field_soccer.establishment.phone,
-                    'establishment_type_dist': reserv.field_soccer.establishment.type_dist.complete,
-                    'latitude': str(reserv.field_soccer.establishment.latitude).replace(',', '.'),
-                    'longitude': str(reserv.field_soccer.establishment.longitude).replace(',', '.')
-                    }
+#         if timedelta(minutes=10) <= time_difference < timedelta(minutes=11):
+#             context = {
+#                     'email': User.objects.get(id=reserv.created_user),
+#                     'date': reserv.date,
+#                     'start_hour': reserv.start_hour,
+#                     'end_hour': reserv.end_hour,
+#                     'field_soccer_name': reserv.field_soccer.name,
+#                     'field_soccer_number_players': reserv.field_soccer.number_players,
+#                     'field_soccer_price': reserv.field_soccer.price,
+#                     'field_soccer_image_base64': reserv.field_soccer.image_base64.decode("utf-8"),
+#                     'establishment_name': reserv.field_soccer.establishment.name,
+#                     'establishment_location': reserv.field_soccer.establishment.location,
+#                     'establishment_phone': reserv.field_soccer.establishment.phone,
+#                     'establishment_type_dist': reserv.field_soccer.establishment.type_dist.complete,
+#                     'latitude': str(reserv.field_soccer.establishment.latitude).replace(',', '.'),
+#                     'longitude': str(reserv.field_soccer.establishment.longitude).replace(',', '.')
+#                     }
             
-            # Send Email
-            email_subject = f'MiCancha - Reserva de campo deportivo para el {reserv.date} de {reserv.start_hour} a {reserv.end_hour}'
-            email_template = 'send_email/reservation.html'
-            email_content = render_to_string(email_template, context)
-            email = EmailMessage(
-                email_subject,
-                email_content,
-                settings.EMAIL_HOST_USER,
-                ['luis.fhb.2016@gmail.com'],
-            )
-            email.content_subtype = 'html'
-            email.send()
+#             # Send Email
+#             email_subject = f'MiCancha - Reserva de campo deportivo para el {reserv.date} de {reserv.start_hour} a {reserv.end_hour}'
+#             email_template = 'send_email/reservation.html'
+#             email_content = render_to_string(email_template, context)
+#             email = EmailMessage(
+#                 email_subject,
+#                 email_content,
+#                 settings.EMAIL_HOST_USER,
+#                 ['luis.fhb.2016@gmail.com'],
+#             )
+#             email.content_subtype = 'html'
+#             email.send()
 
-            # Marcar la reserva como enviada
-            reserv.reminder_sent = True
-            reserv.save()
+#             # Marcar la reserva como enviada
+#             reserv.reminder_sent = True
+#             reserv.save()
 
-        customers.add(reserv.id)
+#         customers.add(reserv.id)
 
     return JsonResponse({
         "status": 200
